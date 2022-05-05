8477acdd3cbb9f9ab0f9ba43f93d8410fe40a5b8
Doiae9 <leonardogh75@gmail.com>
2022-05-04 21:07:09 -0500

    Ejemplo Clases abstractas

---
+++ src/SerVivo.java
@@ -0,0 +1,9 @@
+public abstract class SerVivo {
+    //atributos, getters, setters, constructor
+    public void Comer(){
+        System.out.println("comiendo");
+    }
+    //hablar lo realizan de forma diferente
+    //hablar deberia ser abstracto
+    public abstract void hablar();
+}
